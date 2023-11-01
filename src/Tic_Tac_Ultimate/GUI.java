b71447cd6964f330720ad9f7e1a1aa08a1ba2fa0
AfnanAhmadTariq <116060169+AfnanAhmadTariq@users.noreply.github.com>
2023-11-02 01:30:42 +0500

    v1.0

--- src/Tic_Tac_Ultimate/GUI.java
+++ src/Tic_Tac_Ultimate/GUI.java
@@ -9,6 +9,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 public class GUI extends Application{
@@ -25,6 +26,45 @@ public class GUI extends Application{
         stage.setHeight(720);
         stage.setResizable(false);
         stage.setFullScreen(true);
+
+//        Line line = new Line();
+//        line.setStartY(0);
+//        line.setEndY(stage.getHeight());
+//        line.setStartX(Math.floor(stage.getWidth()/stage.getHeight()));
+//        line.setEndX(Math.floor(stage.getWidth()/stage.getHeight())+stage.getHeight());
+
+        int cell = 600 / 9;
+
+        for (int row = 0; row <= 9; row++) {
+            int y = (row * cell) + 500;
+            Line line = new Line();
+            line.setStartY(100);
+            line.setEndY(700);
+            line.setStartX(y);
+            line.setEndX(y);
+            if(row == 3 || row == 6 || row == 9) {
+                line.setStrokeWidth(3);
+            }
+            else{
+                line.setStrokeWidth(1.5);
+            }
+            root.getChildren().add(line);
+        }
+        for (int col = 0; col <= 9; col++) {
+            int x = (col * cell) + 100;
+            Line line = new Line();
+            line.setStartY(x);
+            line.setEndY(x);
+            line.setStartX(500);
+            line.setEndX(1100);
+            if(col == 3 || col == 6 || col == 9) {
+                line.setStrokeWidth(5);
+            }
+            else{
+                line.setStrokeWidth(1.5);
+            }
+            root.getChildren().add(line);
+        }
 
 
         stage.setScene(scene);
