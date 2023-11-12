b9bb10fedc7cf38297816b19f1302a68b82c54df
AfnanAhmadTariq <116060169+AfnanAhmadTariq@users.noreply.github.com>
2023-11-12 18:17:18 +0500

    v2.6.0

--- src/Tic_Tac_Ultimate/GUI.java
+++ src/Tic_Tac_Ultimate/GUI.java
@@ -4,6 +4,7 @@ import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
@@ -13,8 +14,7 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import javafx.scene.text.Text;
@@ -30,7 +30,7 @@ import static Tic_Tac_Ultimate.Tic_Tac_Ultimate.*;
 public class GUI extends Application  {
     private Scene game ;
     public static Group marks;
-    public static int cell = 600/3;
+    public static double cell;
     private static Color backGround = Color.web("#f2f2f2");
     private static Color midGround = Color.web("#fff");
 
@@ -49,33 +49,35 @@ public class GUI extends Application  {
     }
     public void displayStart(Stage stage) throws Exception {
         HBox root = new HBox();
+        root.setBackground(new javafx.scene.layout.Background(
+                new javafx.scene.layout.BackgroundFill(Color.BLACK, null, null)));
         Scene scene = new Scene(root, midGround);
-//        Group simple = new Group(new Text(""));
-//        simple.set
-//        Group ultimate = new Group(new Text(""));
-//        root.getChildren().add(simple);
-//        root.getChildren().add(ultimate);
 
         Rectangle simple = new Rectangle();
         simple.setFill(Color.LIGHTBLUE);
-        simple.setWidth(780);
+        simple.widthProperty().bind(root.widthProperty().divide(2));
         simple.setHeight(stage.getHeight());
         System.out.println("this is width: " + stage.getWidth());
-
-        Text text = new Text("Tic Tac Toe");
-
+//
+//        Text text = new Text("Tic Tac Toe");
+//
         root.getChildren().add(simple);
-
-
+//
+//
         Rectangle ultimate = new Rectangle();
         ultimate.setFill(Color.BLUE);
-        ultimate.setWidth(stage.getWidth());
+        ultimate.widthProperty().bind(root.widthProperty().divide(2));
         ultimate.setHeight(stage.getHeight());
 
         Text text2 = new Text("Super Tic Tac Toe");
 
         root.getChildren().add(ultimate);
 
+//        //Set the width of the content pane to half the width of the stage
+//        stage.widthProperty().addListener((obs, oldWidth, newWidth) -> {
+//            double halfWidth = newWidth.doubleValue() / 2.0;
+//            simple.setWidth(halfWidth);
+//        });
 
         stage.setScene(scene);
         stage.show();
@@ -85,25 +87,37 @@ public class GUI extends Application  {
     }
     private void displayPlay(Stage stage) throws Exception{
 
-        Group root = new Group();
-
+        BorderPane root = new BorderPane();
         game =  new Scene(root, backGround);
-//        Rectangle rectangle = new Rectangle();
-//        rectangle.setFill(Color.LIGHTBLUE);
-//        rectangle.setWidth(200);
-//        rectangle.setHeight(200);
-//
-//        Text text = new Text("Centered Square");
-//
-//        root.getChildren().add(rectangle);
-//        root.getChildren().add(text);
-//
-//        setAlignment(Pos.CENTER);
-        superTicTacToe(root);
+        StackPane board = new StackPane();
+        root.setCenter(board);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setFill(midGround);
+        rectangle.widthProperty().bind(root.heightProperty().multiply(0.95));
+        rectangle.heightProperty().bind(root.heightProperty().multiply(0.95));
+        rectangle.setArcWidth(50);
+        rectangle.setArcHeight(50);
+
+
+        Pane grid = new Pane();
+        grid.maxWidthProperty().bind(root.heightProperty().multiply(0.8));
+        grid.maxHeightProperty().bind(root.heightProperty().multiply(0.8));
+        Platform.runLater(() -> {
+            cell = grid.getHeight()/3;
+            System.out.println("cell: " + cell);
+        });
+
+        grid.setBackground(new javafx.scene.layout.Background(
+                new javafx.scene.layout.BackgroundFill(midGround, null, null)));
+        superTicTacToe(grid);
+
+        board.getChildren().add(rectangle);
+        board.getChildren().add(grid);
 
         stage.setScene(game);
         stage.show();
-        Toss();
+//        Toss();
     }
     private void displayOptions(Stage stage) throws Exception{
 
@@ -172,8 +186,8 @@ public class GUI extends Application  {
 //        marks = new Group();
 //        root.getChildren().add(marks);
 //    }
-    private void ticTacToe(Group root) throws Exception{
-
+    private void ticTacToe(Pane grid) throws Exception{
+        Platform.runLater(() -> {
 //        root.set
 //        root.setFillHeight(true);
 //        root.setBackground(new Background(new BackgroundFill(midGround, CornerRadii.EMPTY, Insets.EMPTY)));
@@ -185,105 +199,108 @@ public class GUI extends Application  {
 //        line.setEndX(Math.floor(stage.getWidth()/stage.getHeight())+stage.getHeight());
 
 
-        game.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                System.out.println("mouse Clicked!!");
-                int x = (int) event.getX();
-                int y = (int) event.getY();
-                int j = (x-500)/cell;
-                int i = (y-100)/cell;
-                if(getPlayer()!=2 || !isSinglePlayer()){
-                    if(turn(i, j))
-                        System.out.println("registered!!...............");
+            game.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    System.out.println("mouse Clicked!!");
+                    double x = event.getX();
+                    double y = event.getY();
+                    int j = (int) (x/cell);
+                    int i = (int) (y/cell);
+                    if(getPlayer()!=2 || !isSinglePlayer()){
+                        if(turn(i, j))
+                            System.out.println("registered!!...............");
+                    }
                 }
+            });
+            for (int row = 0; row < 4; row++) {
+                double y = row * cell;
+                Line line = new Line();
+                line.setStartY(0);
+                line.setEndY(grid.getHeight());
+                line.setStartX(y);
+                line.setEndX(y);
+                line.setStrokeWidth(5);
+                line.setPickOnBounds(false);
+                grid.getChildren().add(line);
             }
+            for (int col = 0; col < 4; col++) {
+                double x = col * cell;
+                Line line = new Line();
+                line.setStartY(x);
+                line.setEndY(x);
+                line.setStartX(0);
+                line.setEndX(grid.getWidth());
+                line.setStrokeWidth(5);
+                line.setPickOnBounds(false);
+                grid.getChildren().add(line);
+            }
+            marks = new Group();
+            grid.getChildren().add(marks);
         });
-        for (int row = 0; row < 4; row++) {
-            int y = (row * cell) + 500;
-            Line line = new Line();
-            line.setStartY(100);
-            line.setEndY(700);
-            line.setStartX(y);
-            line.setEndX(y);
-            line.setStrokeWidth(5);
-            line.setPickOnBounds(false);
-            root.getChildren().add(line);
-        }
-        for (int col = 0; col < 4; col++) {
-            int x = (col * cell) + 100;
-            Line line = new Line();
-            line.setStartY(x);
-            line.setEndY(x);
-            line.setStartX(500);
-            line.setEndX(1100);
-            line.setStrokeWidth(5);
-            line.setPickOnBounds(false);
-            root.getChildren().add(line);
-        }
-        marks = new Group();
-        root.getChildren().add(marks);
     }
-    private void superTicTacToe(Group root) throws Exception{
-        game.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                System.out.println("mouse Clicked!!");
-                int x = (int) event.getX();
-                int y = (int) event.getY();
-                int j = ((x-500)/(cell/3))%3;
-                int sJ = (x-500)/cell;
-                int i = ((y-100)/(cell/3))%3;
-                int sI = (y-100)/cell;
-                if(getPlayer()!=2 || !isSinglePlayer()){
-                    Tic_Tac_Ultimate.turn(i, j, sI, sJ);
+    private void superTicTacToe(Pane grid) throws Exception{
+        Platform.runLater(() -> {
+            game.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    System.out.println("mouse Clicked!!");
+                    double x = event.getX();
+                    double y = event.getY();
+                    int j = (int) (x  / (cell / 3)) % 3;
+                    int sJ = (int) (x  / cell);
+                    int i = (int) (y  / (cell / 3)) % 3;
+                    int sI = (int) (y  / cell);
+                    if (getPlayer() != 2 || !isSinglePlayer()) {
+                        Tic_Tac_Ultimate.turn(i, j, sI, sJ);
+                    }
                 }
+            });
+            for (int row = 0; row < 10; row++) {
+                double y = row * (cell / 3);
+                Line line = new Line();
+                line.setStartY(0);
+                line.setEndY(grid.getHeight());
+                line.setStartX(y);
+                line.setEndX(y);
+                if (row == 0 || row == 3 || row == 6 || row == 9)
+                    line.setStrokeWidth(8);
+                else
+                    line.setStrokeWidth(3);
+                line.setPickOnBounds(false);
+                grid.getChildren().add(line);
             }
+            for (int col = 0; col < 10; col++) {
+                double x = col * (cell / 3);
+                Line line = new Line();
+                line.setStartY(x);
+                line.setEndY(x);
+                line.setStartX(0);
+                line.setEndX(grid.getWidth());
+                if (col == 0 || col == 3 || col == 6 || col == 9)
+                    line.setStrokeWidth(8);
+                else
+                    line.setStrokeWidth(3);
+                line.setPickOnBounds(false);
+                grid.getChildren().add(line);
+            }
+            marks = new Group();
+            grid.getChildren().add(marks);
         });
-        for (int row = 0; row < 10; row++) {
-            int y = (row * (cell/3)) + 500;
-            Line line = new Line();
-            line.setStartY(100);
-            line.setEndY(700);
-            line.setStartX(y);
-            line.setEndX(y);
-            if(row==0 || row==3 || row==6 || row==9)
-                line.setStrokeWidth(8);
-            else
-                line.setStrokeWidth(3);
-            line.setPickOnBounds(false);
-            root.getChildren().add(line);
-        }
-        for (int col = 0; col < 10; col++) {
-            int x = (col * (cell/3)) + 100;
-            Line line = new Line();
-            line.setStartY(x);
-            line.setEndY(x);
-            line.setStartX(500);
-            line.setEndX(1100);
-            if(col==0 || col==3 || col==6 || col==9)
-                line.setStrokeWidth(8);
-            else
-                line.setStrokeWidth(3);
-            line.setPickOnBounds(false);
-            root.getChildren().add(line);
-        }
-        marks = new Group();
-        root.getChildren().add(marks);
     }
     public static void showTurn(int i, int j, int[] superIndex){
-        i = (i*(cell/3))+(superIndex[0]*cell)+105;
-        j = (j*(cell/3))+(superIndex[1]*cell)+505;
+        i = (int) ((int) (i*(cell/3))+(superIndex[0]*cell));
+        j = (int) ((int) (j*(cell/3))+(superIndex[1]*cell));
         showMark(i,j,true,true, false);
     }
     public static void showTurn(int i, int j){
-        i = (i*cell)+150;
-        j = (j*cell)+550;
+        i = (int)(i*cell)+150;
+        j = (int)(j*cell)+550;
         showMark(i,j,true,false,false);
     }
     public static void markDraw(int[] superIndex){
-        int i = (superIndex[1]*cell)+100;
-        int j = (superIndex[0]*cell)+500;
+        int i = (int)(superIndex[1]*cell)+100;
+        int j = (int)(superIndex[0]*cell)+500;
         showMark(i,j,false,false,true);
     }
     private static void showMark(int i, int j, boolean win, boolean small, boolean dark){
@@ -298,7 +315,7 @@ public class GUI extends Application  {
         mark = !dark? win? getPlayer()==1? p1Icon : p2Icon : dIcon : darkIcon;
         System.out.println("Mark set acc to player!");
         int width, height;
-        width = height = !dark? small? 50 : 100 : cell;
+        width = height = !dark? small? 50 : 100 :(int) cell;
 
         ImageView imageView = new ImageView(mark);
         imageView.setFitWidth(width);
@@ -312,24 +329,24 @@ public class GUI extends Application  {
     }
     public static void markLine(int value){
         int[][] lineIndex = dictionary.get(value);
-        int startX = ((lineIndex[0][1]*cell)+(int)(cell*2.8));
-        int startY = ((lineIndex[0][0]*cell)+cell);
-        int endX = ((lineIndex[2][1]*cell)+(int)(cell*3.2));
-        int endY = ((lineIndex[2][0]*cell)+cell);
+        int startX = (int)((lineIndex[0][1]*cell)+(int)(cell*2.8));
+        int startY =(int) ((lineIndex[0][0]*cell)+cell);
+        int endX = (int)((lineIndex[2][1]*cell)+(int)(cell*3.2));
+        int endY = (int)((lineIndex[2][0]*cell)+cell);
 
         markLine(startX,startY,endX,endY);
     }
     public static void markLine(int[] superIndex,int value){
         int[][] lineIndex = dictionary.get(value);
-        int startX = ((lineIndex[0][1]*(cell/3))+(int)((cell/3)*2.8)+superIndex[1]*cell);
-        int startY = ((lineIndex[0][0]*(cell/3))+(cell/3)+superIndex[0]*cell);
-        int endX = ((lineIndex[2][1]*(cell/3))+(int)((cell/3)*3.2)+superIndex[1]*cell);
-        int endY = ((lineIndex[2][0]*(cell/3))+(cell/3)+superIndex[0]*cell);
+        int startX =(int) ((lineIndex[0][1]*(cell/3))+(int)((cell/3)*2.8)+superIndex[1]*cell);
+        int startY =(int) ((lineIndex[0][0]*(cell/3))+(cell/3)+superIndex[0]*cell);
+        int endX =(int) ((lineIndex[2][1]*(cell/3))+(int)((cell/3)*3.2)+superIndex[1]*cell);
+        int endY =(int) ((lineIndex[2][0]*(cell/3))+(cell/3)+superIndex[0]*cell);
 
         markLine(startX,startY,endX,endY);
 
-        int i = (superIndex[1]*cell)+150;
-        int j = (superIndex[0]*cell)+550;
+        int i =(int) (superIndex[1]*cell)+150;
+        int j =(int) (superIndex[0]*cell)+550;
         showMark(i,j,true,false,true);
     }
     public static void markLine(int startX, int startY, int endX, int endY){
